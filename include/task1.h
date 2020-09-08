11f2c488051f9369d62b65fecac0b3968c561c8f
Nikita Ziganshin <63304439+Liberator-1775@users.noreply.github.com>
2020-09-08 20:23:13 +0300

    Зиганшин Никита

---
+++ include/task1.h
@@ -0,0 +1,12 @@
+//
+// Created by Nikita on 08.09.2020.
+//
+
+#ifndef INC_08_09_TASK1_H
+#define INC_08_09_TASK1_H
+
+#include <iostream>
+
+unsigned int getPrime(unsigned int n);
+
+#endif //INC_08_09_TASK1_H

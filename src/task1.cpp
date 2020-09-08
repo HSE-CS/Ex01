163d656c63ca4a5839904be386cd5eb5ae721e4d
Dev-will-work <63115710+Dev-will-work@users.noreply.github.com>
2020-09-08 22:14:09 +0300

    Update task1.cpp
    expanded the horizons

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
 unsigned int getPrime(unsigned int n) {
-    int* a = new int[2000];
+    int* a = new int[10000];
     int number = 0;
-    for (int i = 0; i < 2000; i++)
+    for (int i = 0; i < 10000; i++)
     {
         a[i] = i;
     }
     a[1] = 0;
-    for (int p = 2; p < 2000; p++)
+    for (int p = 2; p < 10000; p++)
     {
         if (a[p] != 0)
         {
-            for (int j = p * p; j < 2000; j += p) {//обнуляем все числа, кратные данному
+            for (int j = p * p; j < 10000; j += p) {//обнуляем все числа, кратные данному
                 a[j] = 0;
             }
         }
     }
-    for (int i = 0; i < 2000 && n > 0; i++) {
+    for (int i = 0; i < 10000 && n > 0; i++) {
         if (a[i] != 0) {
             n--;
             number = a[i];

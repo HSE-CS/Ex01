3adfcef351f464f86b6c055d6af70f399fe8d62e
LexaLexarLexarex <idlexar@gmail.com>
2020-09-08 12:36:23 +0300

    comment

---
+++ include/task1.h
@@ -0,0 +1,1 @@
+unsigned int getPrime(int n);
---
+++ src/task1.cpp
@@ -0,0 +1,15 @@
+unsigned int getPrime(int n)
+{
+    int k = 1;
+    for (int i = 0, now = 0; i<n; now ++){
+        for (int j = 2; j <= sqrt(now); j++)
+        if(now%j!=0 && now!=1){
+            i++;
+            k = now;
+        }         
+    }
+    return k;
+}
+
+
+	

42eb7c4dedef58c97b7a630448c69b901fd459e1
LukichevaPolina <lukichevapolina42@gmail.com>
2020-09-08 17:24:52 +0300

    check Travis

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,22 +1,31 @@
 #include "task1.h"
 unsigned int task1::getPrime(unsigned int n)
 {
-	int count = 0, i = 2;
-	while (count != n) {
-		int f = 0;
-		for (int j = 2; j < i; j++)
-		{
-			if (i % j == 0) {
-				f = 1;
-				break;
+	unsigned int searchedPrime = 0, counter = 1, currentNumber = 2, i = 0, passed = 1;
+	unsigned int* primeNumbers;
+
+	primeNumbers = (unsigned int*)calloc(n, sizeof(unsigned int));
+	primeNumbers[0] = 2;
+
+	while (counter < n) {
+		passed = 1;
+		i = 0;
+		while (i < counter && passed) {
+			if (currentNumber % primeNumbers[i] == 0 || currentNumber == primeNumbers[i]) {
+				passed = 0;
 			}
+			i++;
 		}
-		if (!f)
-			count++;
-		++i;
-		
-	
+		if (passed) {
+			primeNumbers[counter] = currentNumber;
+			counter++;
+		}
+		currentNumber++;
 	}
-	return i-1;
+
+	searchedPrime = primeNumbers[counter - 1];
+	free(primeNumbers);
+
+	return searchedPrime;
 }
 

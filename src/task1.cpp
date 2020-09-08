fb560498214d2c3c5dd5350ea1dda001c77c6a97
Stolbov Yaroslav <stojar2001@gmail.com>
2020-09-08 15:26:31 +0300

    Stolbov Yaroslav 19Pi-2

---
+++ src/task1.cpp
@@ -0,0 +1,22 @@
+//
+// Created by stoja on 08.09.2020.
+//
+#include "iostream"
+using namespace std;
+unsigned int getPrime(unsigned int n){
+    int  j, a;
+    a = 0;
+    for (int p = 2;; p++) {
+        for (j = 2; j * j <= p; j++)
+            if ((p % j) == 0) break;
+        if (j * j > p) {
+            a++;
+            if (a == n) {
+                cout << p << endl;
+                break;
+            }
+        }
+
+    }
+    return 0;
+}

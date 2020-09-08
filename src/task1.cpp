657e8e87d968f78ecd907b78123c0b9f9fe46670
LexaLexarLexarex <idlexar@gmail.com>
2020-09-08 13:24:45 +0300

    ROMAN BURTSEV

---
+++ src/task1.cpp
@@ -0,0 +1,35 @@
+#include"task1.h"
+#include<iostream>
+#include<vector>
+
+using namespase std;
+
+unsigned int getPrime(int n)
+{
+    int l = 1, m, i=2;
+    vector<int>numbers(100000);
+    for(int j = 0;j<100000;j++){
+        numbers[j] = 0;
+    }
+    if (n == 1)
+        return 1;
+    while(1){
+        if(i == 100000){
+            return 0;
+        }
+        if(numbers[i] == 0){
+            for(int j = i;j<100000;j = j+i){
+                numbers[j] = 1;
+            }
+            l++;
+            m = i;
+        }
+        if(l == n){
+            return m;
+        }
+        i++;
+    }
+}
+
+
+	

1d9b33e21ad2dcfb3e4852fecffc181801c1873d
Julia Rogozyan <juliarogozyan@MacBook-Air-Julia.local>
2020-09-14 15:42:31 +0300

    Rogozyan Anastasiya

---
+++ include/task1.hpp
@@ -0,0 +1,11 @@
+//
+//  task1.hpp
+//  task1.h
+//
+//  Created by Julia Rogozyan on 14.09.2020.
+//  Copyright © 2020 Julia Rogozyan. All rights reserved.
+//
+
+
+unsigned int getPrime(unsigned int n);
+

c3de33f315273eff981fe83ab1b90949727d56a5
Ivanov Daniil <daivanov_6@edu.hse.ru>
2020-09-14 13:59:40 +0300

    Initial commit

---
+++ include/task1.hpp
@@ -0,0 +1,15 @@
+//
+//  task1.hpp
+//  programming-technologies
+//
+//  Created by Даниил Иванов on 14.09.2020.
+//  Copyright © 2020 Daniil Ivanov. All rights reserved.
+//
+
+#ifndef task1_hpp
+#define task1_hpp
+
+#include <stdio.h>
+
+unsigned int getPrime(unsigned int n);
+#endif /* task1_hpp */
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  programming-technologies
+//
+//  Created by Даниил Иванов on 14.09.2020.
+//  Copyright © 2020 Daniil Ivanov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

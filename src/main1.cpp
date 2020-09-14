072f7e988d25e00f32d8437c623a063e37aa365c
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2020-09-14 16:25:15 +0300

    b103

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,1 +1,10 @@
-unsigned int getPuime?(unsigned int n)
+#include "task1.h"
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    cout<<getPrime(n)<<endl;
+    return 0;
+}

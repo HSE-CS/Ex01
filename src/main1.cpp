817d2008fb9cf13786faffd4ae5532a0832aa35d
Your NameVadukk <Vadik-Tumakov.com@yandex.ru>
2020-09-14 15:39:05 +0300

    xjshfbskjdf

---
+++ src/main1.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "task1.h"
+#include<locale.h> 
+#include < conio.h >
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int N;
+	printf("\nВведите целое число ");
+	scanf("%d", &N);
+	if (getPrime(N))
+		printf("Число %d - простое\n", N);
+	else printf("Число %d - составное\n", N);
+	return 0;
+}

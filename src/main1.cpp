9d8b6cd38609c61ee5d0f0a90e375ff727af1b55
Right-bugor <innokentiyyyyy@gmail.com>
2020-09-14 15:26:46 +0300

    pervai program

---
+++ src/main1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "task1.h"
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << getPrime(n);
+	return 0;
+}
---
+++ src/task1.cpp
@@ -0,0 +1,5 @@
+#include "task1.h"
+
+unsigned int getPrime(unsigned int n) {
+	return 3;
+}

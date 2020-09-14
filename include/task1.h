e866c9fbe6562f51f6572908838e3d17d7c98f15
Schterben <dsbarinov_1@edu.hse.ru>
2020-09-14 22:18:44 +0300

    blablabla

--- include/task1.h
+++ include/task1.h
@@ -1,1 +1,6 @@
+#ifndef TASK1_H
+#define TASK1_H
+
 unsigned int getPrime(unsigned int n);
+
+#endif
--- src/task1.cpp
+++ src/task1.cpp
@@ -2,6 +2,24 @@
 
 unsigned int getPrime(unsigned int n)
 {
-	n = 2;
-	return(n);
+    const int SIZE = 100 * 1000;
+    const int N = 9500;
+
+    int a[SIZE];
+    for (int i = 2; i < SIZE; ++i)
+        a[i] = 1;
+
+    int k = 1;
+    int p = 2;
+    while (k < n) {
+        for (int i = 2 * p; i < SIZE; i += p)
+            a[i] = 0;
+        for (int i = p + 1; i < SIZE; ++i)
+            if (a[i] == 1) {
+                p = i;
+                break;
+            }
+        ++k;
+    }
+    return p;
 }

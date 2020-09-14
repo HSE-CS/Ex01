7cc4693663d87519d161a523283c3c495589e3f1
Windows 7 <student7@std.nnov.corp.local>
2020-09-14 14:24:33 +0300

    Добавала файлы

---
+++ include/task1.h
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef TASK1_H
+#define TASK1_H
+
+unsigned int getPrime(unsigned int n);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Task1.h"
+#include <stdio.h>
+
+int main() {
+
+	getPrime(1);
+	return 0;
+}

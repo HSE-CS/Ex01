518edb47222b1ed9c2191b0cd2b9bc763f1651b3
Karina <karmen.01@inbox.ru>
2020-09-14 21:17:41 +0300

    Second

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,19 +1,9 @@
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 #include "task1.h"
 
 
 
 int main() {
-
-	int n = 0;
-	printf("Enter number: ");
-	scanf("%d", &n);
-	printf("%d", getPrime(n));
-	_getch();
 	return 0;
 
 	

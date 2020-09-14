8b075955d4fe1d04e57da401d61c613239a4febc
alexgiving <alexgiving@mail.ru>
2020-09-14 15:44:19 +0300

    Make some changes

--- include/task1.h
+++
@@ -1,6 +0,0 @@
-#ifndef task1_h
-#define task1_h
-
-unsigned int getPrime(unsigned int n);
-
-#endif
--- src/task1.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/task1.h"
-
-unsigned int getPrime(unsigned int n){
-    n = 0;
-}

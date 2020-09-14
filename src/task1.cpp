8a014b545855c7b42991a33f82c7bd90f72cb42f
ch2oh-ch2oh <valerasofronov01@gmail.com>
2020-09-14 15:48:45 +0300

    test

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,3 +1,9 @@
+int simple(int n){
+    for(int i=2;i<= n/2;i++) 
+        if( (n%i)==0 ) return 0;
+            return 1;
+}
+
 unsigned int getPrime(unsigned int n){
     int k=0;
     for(int i=2;;i++)    
@@ -9,8 +15,3 @@ unsigned int getPrime(unsigned int n){
     return 1;
 }
 
-int simple(int n){
-    for(int i=2;i<= n/2;i++) 
-        if( (n%i)==0 ) return 0;
-            return 1;
-}

2baf81763778c7e01324b056b2dc4bdb7246b877
Irina <pasmanikirina29@gmail.com>
2020-09-14 15:28:24 +0300

    ура получилось

---
+++ include/task1.h
@@ -0,0 +1,6 @@
+#ifndef NODE_H
+#define NODE_H
+
+unsigned int get_prime(int n);
+
+#endif	
---
+++ src/task1.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "task1.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <math.h>
+
+unsigned int get_prime(int n) {
+	int prime = n;
+	return prime;
+}

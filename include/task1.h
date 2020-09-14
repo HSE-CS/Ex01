e495a38b97b005d11928ff2cb96e1b6d20cdb0a1
KuznetsovMikhail <KuznetsovMikhail>
2020-09-14 15:37:14 +0300

    Должно работать)

---
+++ include/task1.h
@@ -0,0 +1,22 @@
+#ifndef TASK1_H_INCLUDED
+#define TASK1_H_INCLUDED
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <stack>
+#include <malloc.h>
+unsigned int getPrime(unsigned int n);
+
+
+#endif // TASK1_H_INCLUDED
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <stack>
+#include <malloc.h>
+#include "task1.h"
+
+using namespace std;
+
+
+int main(){
+    unsigned int n;
+    cout << "Put n\n";
+    cin >> n;
+    cout << getPrime(n);
+    return 0;
+}

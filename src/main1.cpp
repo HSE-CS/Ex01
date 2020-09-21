dfad6551b8c5762c2918dceb4d03728c461f912d
NAKAUTT <khorev.egor@list.ru>
2020-09-21 13:55:50 +0300

    123

---
+++ src/main1.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  programming-technologies
+//
+//  Created by Егор Хорев on 14.09.2020.
+//  Copyright © 2020 Egor Khorev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

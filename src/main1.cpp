ce99afda59a63cca1d66a08b1a143e2fdd6117be
burgundypie <kotyonok2001@mai.ru>
2020-10-12 14:21:27 +0300

    Симонова Арина

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< HEAD
 #include "task1.h"
 using namespace std;
 
@@ -8,4 +9,9 @@ int main()
 	cin >> n;
 	cout << getPrime(n) << endl;
 	return 0;
+=======
+int main
+{
+	std::count <<"Hello World";
+>>>>>>> cf91cf02be26a542450e80d5805a9e5ce99e520a
 }

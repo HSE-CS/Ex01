596369e2418116f75fb0477993e65eeb7e464c26
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-10-13 12:42:01 +0300

    Tha last

--- src/task1.cpp
+++ src/task1.cpp
@@ -6,11 +6,11 @@ using namespace std;
 unsigned int getPrime(unsigned int n) {
     unsigned int count = {0};
     bool found = false;
-    int num = {0};
-    int k = {2};
-    while( !found) {
+    unsigned int num = {0};
+    unsigned int k = {2};
+    while(!found) {
         int dels = 0;
-        for(int i = {2}; i < k; i++) if(0 == k % i) dels++;
+        for(unsigned int i = {2}; i < k; i++) if(0 == k % i) dels++;
         if(0 == dels) {
             count++;
             if(count == n) {
